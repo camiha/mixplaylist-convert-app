7cc00580b4da6b8fa1f50324bda8fbee5d9ec440
cyamy <hi.cyamy@gmail.com>
2023-07-17 00:53:35 +0900

    format

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from './Button';
-import './header.css';
+import { Button } from "./Button";
+import "./header.css";
 
 type User = {
   name: string;
@@ -14,11 +14,21 @@ interface HeaderProps {
   onCreateAccount: () => void;
 }
 
-export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
+export const Header = ({
+  user,
+  onLogin,
+  onLogout,
+  onCreateAccount,
+}: HeaderProps) => (
   <header>
     <div className="storybook-header">
       <div>
-        <svg height="32" viewBox="0 0 32 32" width="32" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          height="32"
+          viewBox="0 0 32 32"
+          width="32"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <g fill="none" fillRule="evenodd">
             <path
               d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
@@ -47,7 +57,12 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
         ) : (
           <>
             <Button label="Log in" onClick={onLogin} size="small" />
-            <Button label="Sign up" onClick={onCreateAccount} primary size="small" />
+            <Button
+              label="Sign up"
+              onClick={onCreateAccount}
+              primary
+              size="small"
+            />
           </>
         )}
       </div>
