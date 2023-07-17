957548204e678917f03f9bed549d59ad6800ffee
cyamy <hi.cyamy@gmail.com>
2023-07-17 18:27:28 +0900

    fix header's padding

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,7 @@ import { Logo } from "../logo";
 export const Header = () => {
   return (
     <Box bg="green.primary">
-      <Flex alignItems="center" maxWidth="container.lg" mx="auto" padding={2}>
+      <Flex alignItems="center" maxWidth="container.lg" mx="auto" px={4} py={2}>
         <Logo level="h2" />
         <Spacer />
         <Button fill={false}>Menu=&gt;</Button>
