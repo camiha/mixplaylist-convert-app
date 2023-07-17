1a31f4e240f779a620116b87a60931755c07c190
cyamy <hi.cyamy@gmail.com>
2023-07-17 14:53:05 +0900

    add link variants

--- src/components/link/linkInner.stories.tsx
+++ src/components/link/linkInner.stories.tsx
@@ -6,6 +6,15 @@ const meta: Meta<typeof LinkInner> = {
   title: "Components / Navigation / Link",
   component: LinkInner,
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div
+        style={{ backgroundColor: "rgba(221, 221, 221, 0.5)", padding: "16px" }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default meta;
@@ -14,6 +23,20 @@ type Story = StoryObj<typeof LinkInner>;
 
 export const Primary: Story = {
   args: {
+    variant: "primary",
+    children: "Login=>",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+    children: "Login=>",
+  },
+};
+export const Box: Story = {
+  args: {
+    variant: "box",
     children: "Login=>",
   },
 };
--- src/components/link/linkInner.tsx
+++ src/components/link/linkInner.tsx
@@ -2,28 +2,36 @@ import { ReactNode } from "react";
 
 import { Link as ChakraLink } from "@chakra-ui/react";
 import NextLink from "next/link";
+
 // TODO: add hover color
 const variants = {
   primary: {
     color: "black.primary",
+    fontSize: 4,
+  },
+  secondary: {
+    color: "white.primary",
+    fontSize: 4,
+  },
+  box: {
+    color: "black.primary",
     borderColor: "yellow.primary",
     backgroundColor: "yellow.primary",
+    fontStyle: "italic",
+    fontWeight: "bold",
+    px: 1,
+    fontSize: 6,
   },
 };
 export type LinkProps = {
   children: ReactNode;
   nextLink?: typeof NextLink;
+  variant: keyof typeof variants;
 };
 
-export const LinkInner = ({ nextLink, children }: LinkProps) => {
+export const LinkInner = ({ nextLink, children, variant }: LinkProps) => {
   return (
-    <ChakraLink
-      as={nextLink}
-      {...variants.primary}
-      fontStyle="italic"
-      fontWeight="bold"
-      px={1}
-    >
+    <ChakraLink as={nextLink} {...variants[variant]}>
       {children}
     </ChakraLink>
   );
