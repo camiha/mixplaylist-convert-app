8d7a2a22c0c4954904d726b992fd6af7a57523e8
camiha <hello.camiha@gmail.com>
2023-07-30 16:37:20 +0900

    add spotify api client

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-import { API_URL } from "@/config/constants";
+import { API_URL, SPOTIFY_API_URL } from "@/config/constants";
 
 export const apiClient = Axios.create({
   baseURL: API_URL,
@@ -8,7 +8,26 @@ export const apiClient = Axios.create({
     "Content-Type": "application/json",
   },
 });
+
 apiClient.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const message = error.response?.data?.message || error.message;
+    console.error(message);
+    return Promise.reject(error);
+  },
+);
+
+export const spotifyApiClient = Axios.create({
+  baseURL: SPOTIFY_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+spotifyApiClient.interceptors.response.use(
   (response) => {
     return response.data;
   },
